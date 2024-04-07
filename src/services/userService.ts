acd232385b85133781d41e69b0b641d906ac4034
Latica Škubonja <skubonjal@gmail.com>
2024-04-07 23:54:48 +0200

    prettier i petak 5.4.

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,23 +2,26 @@ interface User {
   id: string
   name: string
 }
-
 class UserService {
   private users: User[] = []
-
   constructor() {
     // Initially, let's add some dummy users
     this.users.push({ id: '1', name: 'Alice' })
     this.users.push({ id: '2', name: 'Bob' })
     this.users.push({ id: '3', name: 'Charlie' })
   }
-
   getAllUsers(): User[] {
     return this.users
   }
-
   getUserById(id: string): User | undefined {
     return this.users.find((user) => user.id === id)
+  }
+
+  deleteUserById(id: string): User | undefined {
+    return this.users.splice(
+      this.users.findIndex((user) => user.id === id),
+      1,
+    )[0]
   }
 }
 
