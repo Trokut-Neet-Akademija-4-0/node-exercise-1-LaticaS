ef71c72ef8c5fd5a79364fc034cfeec510f0c837
Latica Škubonja <skubonjal@gmail.com>
2024-04-09 15:04:24 +0200

    pon. 8.4. error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 // src/index.js
 import express, { Express } from 'express'
 import homeRoutes from './routes/homeRoutes'
-//import userRoutes from './routes/userRoutes'
 import productRoutes from './routes/productRoutes'
 import cartRoutes from './routes/cartRoutes'
+import errorHandler from './middlewares/ErrorHandler'
 
 const app: Express = express()
 const port = process.env.PORT || 3000
-
 app.use('/', homeRoutes)
-//app.use('/users', userRoutes)
+// app.use('/users', userRoutes)
 app.use('/products', productRoutes)
 app.use('/cart', cartRoutes)
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
+
+//8.4. Dodan error middleware, odvojeni controlleri, dodane dodatne informacije na cart,
+// i refaktor koda
